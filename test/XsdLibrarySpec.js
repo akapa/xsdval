8bebd0d0ebb581db15106f4d0eedb709e91d5924
Kapa <kapa@sipaksi.net>
2014-02-25 18:05:36 +0100

    XsdLibrary tests almost done.

--- test/XsdLibrarySpec.js
+++ test/XsdLibrarySpec.js
@@ -5,6 +5,8 @@ define(['xsdval/XsdLibrary', 'xsd'], function (XsdLibrary, xsd) {
 		var xsdlib;
 		var xsdns = 'http://www.w3.org/2001/XMLSchema';
 		var testns = 'http://test';
+		var schema = xsd.createDocument('schema', { 'xs': xsdns }, 'xs');
+		schema.documentElement.setAttribute('targetNamespace', testns);
 
 		beforeEach(function () {
 			xsdlib = new XsdLibrary();	
@@ -23,9 +25,6 @@ define(['xsdval/XsdLibrary', 'xsd'], function (XsdLibrary, xsd) {
 		});
 
 		describe("adding new XSD", function() {
-
-			var schema = xsd.createDocument('schema', { 'xs': xsdns }, 'xs');
-			schema.documentElement.setAttribute('targetNamespace', testns);
 
 			it("works", function() {
 
@@ -49,6 +48,87 @@ define(['xsdval/XsdLibrary', 'xsd'], function (XsdLibrary, xsd) {
 		
 			});
 		
+		});
+
+		describe("findElement", function() {
+
+			//setting up a scheme with an element in it
+			var schema2 = xsd.createDocument('schema', { 'xs': xsdns }, 'xs');
+			schema2.documentElement.setAttribute('targetNamespace', testns);
+			var elem = schema2.createElementNS(xsdns, 'element');
+			var name = 'test';
+			elem.setAttribute('name', name);
+			schema2.documentElement.appendChild(elem);
+
+			it("returns null when no element found", function() {
+
+				xsdlib.addItem(schema, testns);
+				expect(xsdlib.findElement(testns, name)).toBeNull();
+
+			});
+
+			it("iterates correctly and calls xsd.findElement", function() {
+
+				xsdlib.addItem(schema, testns);
+				xsdlib.addItem(schema2, testns);
+
+				spyOn(xsd, 'findElement').and.callThrough();
+
+				var res = xsdlib.findElement(testns, name);
+				expect(res).toEqual(jasmine.any(Element));
+				expect(xsd.findElement.calls.count()).toEqual(2);
+				expect(xsd.findElement.calls.allArgs()).toEqual([[schema, name], [schema2, name]]);
+
+			});
+
+		});
+
+		describe("findTypeDefinition", function() {
+
+			//setting up a scheme with an element in it
+			var schema2 = xsd.createDocument('schema', { 'xs': xsdns }, 'xs');
+			schema2.documentElement.setAttribute('targetNamespace', testns);
+			var elem = schema2.createElementNS(xsdns, 'complexType');
+			var name = 'test';
+			elem.setAttribute('name', name);
+			schema2.documentElement.appendChild(elem);
+
+			it("returns null when no element found", function() {
+
+				xsdlib.addItem(schema, testns);
+				expect(xsdlib.findTypeDefinition(testns, name)).toBeNull();
+
+			});
+
+			it("iterates correctly and calls xsd.findTypeDefinition", function() {
+
+				xsdlib.addItem(schema, testns);
+				xsdlib.addItem(schema2, testns);
+
+				spyOn(xsd, 'findTypeDefinition').and.callThrough();
+
+				var res = xsdlib.findTypeDefinition(testns, name);
+				expect(res).toEqual(jasmine.any(Element));
+				expect(xsd.findTypeDefinition.calls.count()).toEqual(2);
+				expect(xsd.findTypeDefinition.calls.allArgs()).toEqual([[schema, name], [schema2, name]]);
+
+			});
+
+		});
+
+		describe("findTypeDefinitionFromNodeAttr", function() {
+
+			/*it("calls xsd.getTypeFromNodeAttr with the right params", function() {
+
+				var elem = schema.createElementNS(xsdns, 'element');
+				elem.setAttributeNS(testns, 'type', 'xs:integer');
+
+				spyOn(xsd, 'getTypeFromNodeAttr');
+				xsdlib.findTypeDefinitionFromNodeAttr(elem, 'type', testns);
+				expect(xsd.getTypeFromNodeAttr).toHaveBeenCalledWith(elem, 'type', testns);
+
+			});*/
+
 		});
 
 	});
