285daa4201c9e14159db58be07216106c4125a09
Kapa <kapa@sipaksi.net>
2014-02-25 15:50:47 +0100

    Added basic karma test.

---
+++ test/test-main.js
@@ -0,0 +1,29 @@
+var tests = [];
+for (var file in window.__karma__.files) {
+  if (window.__karma__.files.hasOwnProperty(file)) {
+    if (/Spec\.js$/.test(file)) {
+      tests.push(file);
+    }
+  }
+}
+
+requirejs.config({
+	baseUrl: '/base/js',
+	paths: {
+		'xsdval': '.',
+		'underscore': 'lib/underscore',
+        'text': 'lib/text',
+        'xsd': 'lib/xsd',
+        'xml': 'lib/xml',
+        'Library': 'lib/Library',
+        'objTools': 'lib/objTools',
+        'wgxpath': 'lib/wgxpath'
+    },
+	shim: {
+		'underscore': {
+			exports: '_'
+		}
+	},
+	deps: tests,
+	callback: window.__karma__.start
+});
